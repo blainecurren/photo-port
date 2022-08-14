fcf5c81f6f37ab1941857addd56d95a5faaf0320
Blaine Curren <blaine.curren@gmail.com>
2022-08-13 19:45:22 -0500

    fixed nav component to display sections

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,14 +8,8 @@ function Nav() {
       description:
         "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    {
-      name: "portraits",
-      description: "Portraits of people in my life",
-    },
-    {
-      name: "food",
-      description: "Delicious delicacies",
-    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
     {
       name: "landscape",
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
@@ -23,42 +17,45 @@ function Nav() {
   ];
 
   const handleClick = () => {
-    return (
-      <header data-testid="header" className="flex-row px-1">
-        <h2>
-          <a href="/">
-            <span role="img" aria-label="camera">
-              📸
-            </span>{" "}
-            Oh Snap!
-          </a>
-        </h2>
-        <nav>
-          <ul className="flex-row">
-            <li className="mx-2">
-              <a href="#about" onClick={() => handleClick()}>
-                About Me
-              </a>
-            </li>
-            <li className="mx-2">
-              <span onClick={() => handleClick()}>Contact</span>
-            </li>
-            {categories.map((category) => (
-              <li className="mx-1" key={category.name}>
-                <span
-                  onClick={() => {
-                    handleClick();
-                  }}
-                >
-                  {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    );
+    console.log("click handled");
   };
+
+  return (
+    <header data-testid="header" className="flex-row px-1">
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about" onClick={() => handleClick()}>
+              About me
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span
+                onClick={() => {
+                  handleClick();
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
