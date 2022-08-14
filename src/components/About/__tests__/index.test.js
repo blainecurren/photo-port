86c04652c8119f4deafb895e095dc652bbb3344d
Blaine Curren <blaine.curren@gmail.com>
2022-08-14 08:42:48 -0500

    added snapshot test and ran successfully

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -12,4 +12,9 @@ describe("About component", () => {
   });
 
   // Second test
+  it("matches snapshot DOM node structure", () => {
+    // render About
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
