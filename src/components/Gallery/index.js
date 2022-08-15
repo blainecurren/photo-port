eb5b3a04fc7c0ad45723c307040ac7bc3138ea89
Blaine Curren <blaine.curren@gmail.com>
2022-08-14 19:26:16 -0500

    updated index.css

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
   const currentCategory = {
@@ -9,8 +11,12 @@ function Gallery(props) {
 
   return (
     <section>
-      <h1>{currentCategory.name}</h1>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.name}</p>
+      <div className="flex-row">
+        <img src={photo} alt="Commercial Example" 
+        className="img-thumbnail mx-1"/>
+      </div>
     </section>
   );
 }
