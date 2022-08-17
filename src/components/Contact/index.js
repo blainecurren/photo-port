9d59804d08e9bcf1db94b93626c7b375adf81ca1
Blaine Curren <blaine.curren@gmail.com>
2022-08-17 12:32:33 -0500

    updated isValid functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,20 +8,35 @@ function ContactForm() {
     message: "",
   });
 
+  const [errorMessage, setErrorMessage] = useState("");
   const { name, email, message } = formState;
 
-  function handleChange(e) {
+  const handleChange = (e) => {
     if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
-      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
     }
 
-    setFormState({ ...formState, [e.target.name]: e.target.value });
-  }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  };
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log(formState);
+
+    console.log("errorMessage", errorMessage);
   }
 
   return (
