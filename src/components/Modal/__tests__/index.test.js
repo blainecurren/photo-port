09581f0a964f8b112155038fe51e61bd24629883
Blaine Curren <blaine.curren@gmail.com>
2022-08-19 12:14:15 -0500

    started modal tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import react from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Modal from "..";
+
+const currentPhoto = {
+  name: "Park Bench",
+  category: "landscape",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+  index: 1,
+};
+
+afterEach(cleanup);
+
+describe('Modal Component', () => {
+    it('renders', () => {
+
+    })
+})
+
+it('matches', () => {
+    
+})
