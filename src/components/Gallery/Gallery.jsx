eb730ee4215958513c38338bcd8810286668f00a
Blaine Curren <blainecurren@MacBook-Pro.local>
2022-08-30 20:06:02 -0500

    started to work on card

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -16,58 +16,62 @@ export default function Gallery(props) {
   return (
     <div className="gallery">
       <header>
-        <h2>
-          <a data-testid="link" href="/">
-            <span role="img" aria-label="camera">
-              {" "}
-              📸
-            </span>{" "}
-            Oh Snap!
-          </a>
-        </h2>
         <nav>
           <ul className="flex-row">
-            <li className="mx-2">
-              <a
-                data-testid="about"
-                href="#about"
-                onClick={() => setContactSelected(false)}
-              >
-                About me
-              </a>
-            </li>
-            <li className={`mx-2 ${contactSelected && "navActive"}`}>
-              <span onClick={() => setContactSelected(true)}>Contact</span>
-            </li>
-            {categories.map((category) => (
-              <li
-                className={`mx-1 ${
-                  currentCategory.name === category.name &&
-                  !contactSelected &&
-                  "navActive"
-                }`}
-                key={category.name}
-              >
-                <span
-                  onClick={() => {
-                    setCurrentCategory(category);
-                    setContactSelected(false);
-                  }}
+            <li className="gallery-header">
+              {/*<li className="mx-2">
+                 <a
+                 data-testid="about"
+                 href="#about"
+                 onClick={() => setContactSelected(false)}
+                 >
+                 About me
+                 </a>
+                 </li>
+                 <li className={`mx-2 ${contactSelected && "navActive"}`}>
+                 make this on click to link contact
+                 <span onClick={() => setContactSelected(true)}>Contact</span> 
+                </li> */}
+              {categories.map((category) => (
+                <li
+                  className={`mx-1 ${
+                    currentCategory.name === category.name &&
+                    !contactSelected &&
+                    "navActive"
+                  }`}
+                  key={category.name}
                 >
-                  {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))}
+                  <span
+                    onClick={() => {
+                      setCurrentCategory(category);
+                      setContactSelected(false);
+                    }}
+                  >
+                    {capitalizeFirstLetter(category.name)}
+                  </span>
+                </li>
+              ))}
+            </li>
           </ul>
         </nav>
       </header>
-      <section>
-        <h1 data-testid="h1tag">
-          {capitalizeFirstLetter(currentCategory.name)}
-        </h1>
-        <p>{currentCategory.description}</p>
-        <ProjectList category={currentCategory.name} />
-      </section>
+      <div className="card">
+        <div className="card-image">
+          <figure class="image is-4by3">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt="placeholder image"
+            ></img>
+          </figure>
+        </div>
+        <section>
+          <h1 data-testid="h1tag">
+            {capitalizeFirstLetter(currentCategory.name)}
+          </h1>
+          <p>{currentCategory.description}</p>
+          <ProjectList category={currentCategory.name} />
+        </section>
+      </div>
     </div>
   );
 }
